5ddbc2eee62a492d58ecb6c101611daf46c65a92
Felipe Fontana <felipefontanaf@gmail.com>
2016-11-06 07:15:46 -0500

    docker

--- api/index.js
+++ api/index.js
@@ -67,6 +67,6 @@ app.get('/bairros/:bairros?', function (req, res) {
 
 
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
 });
